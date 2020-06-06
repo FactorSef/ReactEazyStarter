184f8fd60fb8aa71369783bac7955e4992fa837f
FactorSef <aaykovenko@yandex.ru>
2020-06-06 22:47:51 +0400

    Clearup project

--- app/App.tsx
+++ app/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 
-import './style.scss';
-import './style.less';
-
 const App = (): React.ReactElement => {
-	const [test, setTest] = React.useState('test');
-
-	const handleSetKek = (): void => {
-		setTest('kek');
-	};
-
 	return (
-		<>
-			<h1
-				className="kek"
-				style={{}}
-			>
-				{String.format('{0}, {1}', 'hello', test)}
-			</h1>
-
-			<button onClick={handleSetKek} type="button">set kek</button>
-		</>
+		<h1>ReactEazyStarter</h1>
 	);
 };
 
